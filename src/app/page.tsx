64b0e14533e044747226230c2be771c362c4da75
Ali Hasnain <hafizalihasnain465@gmail.com>
2025-12-16 20:21:34 +0500

    vibe-4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/hafiz.jpg" alt="" width="384" height="512"/>
-  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    
+    <div><figure className="md:flex bg-blue-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-3 rounded-full mx-auto" src="/hafiz.jpg" alt="" width="200" height="512"/>
+  <div className="pt-6 md:p-8 text-right md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
         “Tailwind CSS is the only framework that I've seen scale
@@ -13,10 +14,10 @@ const page = () => {
       </p>
     </blockquote>
     <figcaption className="font-medium">
-      <div className="text-sky-500 dark:text-sky-400">
+      <div className="text-green-500 dark:text-sky-400">
         PAIB
       </div>
-      <div className="text-slate-700 dark:text-slate-500">
+      <div className="text-red-700 dark:text-slate-500">
         Hi,I'm Ali hasnain  
       </div>
     </figcaption>
