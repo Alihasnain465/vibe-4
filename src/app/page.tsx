0ae58367781c1b33f6024d41502c99e83bcc9dc2
Ali Hasnain <hafizalihasnain465@gmail.com>
2025-12-17 13:04:56 +0500

    vibe-4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const page = () => {
   return (
-    
-    <div><figure className="md:flex bg-blue-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-3 rounded-full mx-auto" src="/hafiz.jpg" alt="" width="200" height="512"/>
-  <div className="pt-6 md:p-8 text-right md:text-left space-y-4">
+    <div>import React from 'react'
+
+const page = () = {
+   (
+    <div><figure className="bg-blue-100 rounded-xl p-8 dark:bg-slate-800">
+  <img className="w-24 h-24 rounded-full mx-auto" src="/hafiz.jpg" alt="" width="384" height="512"/>
+  <div className="pt-6 space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
         “Tailwind CSS is the only framework that I've seen scale
@@ -13,16 +16,20 @@ const page = () => {
         and the build size is tiny.”
       </p>
     </blockquote>
-    <figcaption className="font-medium">
-      <div className="text-green-500 dark:text-sky-400">
+    <figcaption className=''>
+      <div>
         PAIB
       </div>
-      <div className="text-red-700 dark:text-slate-500">
-        Hi,I'm Ali hasnain  
+      <div>
+        Staff Engineer, Ali hasnain
       </div>
     </figcaption>
   </div>
-</figure>
+</figure></div>
+  )
+}
+
+export default page
       
     </div>
   )
